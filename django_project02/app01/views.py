0217c3d85cad35005db3c2f777d0092d126f93d8
Yuchen <yucpeng17@outlook.com>
2024-07-07 15:17:54 +0930

    Department ADD and delete

--- django_project02/app01/views.py
+++ django_project02/app01/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from app01 import models
 # Create your views here.
+def depart_list(request):
+    # Get all departments
+    department_list_db = models.Department.objects.all() # query set: object lists
+
+    return render(request, 'depart_list.html', {'department_list': department_list_db})
+
+
+def depart_add(request):
+    if request.method == 'GET':
+        return render(request, 'depart_add.html')
+    
+    #     if request.method == 'POST':
+    depart_to_add = request.POST.get('DepartmentName') #绑定的input的name attribute
+    models.Department.objects.create(title=depart_to_add)
+    return redirect('/depart/list/')
+
+
+def depart_delete(request):
+    # /depart/delete/?id=1
+    # get the id from the url
+    nid = request.GET.get('nid')
+    
+    # delete
+    models.Department.objects.filter(id=nid).delete()
+    return redirect('/depart/list/')
